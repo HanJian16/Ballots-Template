94e7edf6f0d2dec002a0e482d93598dc85ec9327
HanJian16 <jhohanjianpierre@gmail.com>
2024-08-11 23:40:46 -0500

    Creacion del appBar y la generacion de la lista con datos prueba para la home screen

--- frontend/ballots_template_flutter/lib/screens/home_screen.dart
+++ frontend/ballots_template_flutter/lib/screens/home_screen.dart
@@ -6,33 +6,106 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              const SizedBox(height: 20),
-              Container(
-                color: Colors.red,
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      appBar: AppBar(
+        title: const Text("Generador de Recibo"),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Icon(Icons.info_outline),
+          )
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              decoration: const BoxDecoration(color: Colors.white),
+              child: const ListTile(
+                title: Text(
+                  'Opciones',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Text(
-                      "Recibo Rápido 2.0",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Icon(Icons.info_outline)
+                    CustomButton(
+                        icon: Icons.print_disabled,
+                        backgroundColor: Colors.red,
+                        color: Colors.white),
+                    CustomButton(
+                        icon: Icons.format_list_bulleted_add,
+                        backgroundColor: Color(0xFFCCCCCC),
+                        color: Colors.black),
                   ],
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.separated(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return const ListTile(
+                    shape: BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      side: BorderSide(width: 1, color: Color(0xFFCCCCCC)),
+                    ),
+                    tileColor: Color(0xFFCCCCCC),
+                    title: Text(
+                      'Titulo',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Subtitulo',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    leading: Icon(Icons.format_list_bulleted_add),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(height: 10);
+                },
+              ),
+            )
+          ],
         ),
       ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.icon,
+    required this.color,
+    required this.backgroundColor,
+  });
+  final IconData icon;
+  final Color color;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton.filled(
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all(backgroundColor),
+      ),
+      color: color,
+      onPressed: () {},
+      icon: Icon(icon),
     );
   }
 }
