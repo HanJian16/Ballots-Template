53daef0fe89535ebcc27298e31328b14fbd263a7
HanJian16 <jhohanjianpierre@gmail.com>
2024-08-12 13:25:32 -0500

    Modularizando datos de la lista de registro

--- frontend/ballots_template_flutter/lib/routes/app_routes.dart
+++ frontend/ballots_template_flutter/lib/routes/app_routes.dart
@@ -1,13 +1,12 @@
+import 'package:ballots_template_flutter/screens/home_screen.dart';
 import 'package:get/get.dart';
 import 'package:ballots_template_flutter/screens/form_register.dart';
-// import 'package:ballots_template_flutter/screens/home_screen.dart';
-// import 'package:ballots_template_flutter/screens/details_screens.dart';
 
 class AppRoutes {
   static const initial = '/';
   static const details = '/details';
 
   static final routes = [
-    GetPage(name: initial, page: () => const FormRegisterScreen()),
+    GetPage(name: initial, page: () => const HomeScreen()),
   ];
 }
