8e6e480b2505f68c877a00d5b6c0ff9b6d8c68ac
Harurin5671 <frankcito639@gmail.com>
2024-08-19 01:24:41 -0500

    feat: modularize BottomSheet and Dialog widgets
    Refactored BottomSheet and Dialog logic into reusable widgets for better maintainability and reusability across the app.

---
+++ frontend/ballots_template_flutter/lib/widgets/warning_dialog.dart
@@ -0,0 +1,42 @@
+import 'package:ballots_template_flutter/widgets/custom_btn.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WarningDialog extends StatelessWidget {
+  const WarningDialog(
+      {super.key,
+      required this.title,
+      required this.onPress,
+      required this.titleBtn});
+  final String title;
+  final String titleBtn;
+  final VoidCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Column(
+        children: [
+          const Icon(Icons.warning_sharp, size: 55, color: Color(0xFFff8248)),
+          const SizedBox(height: 10),
+          Text(
+            title,
+            style: GoogleFonts.onest(
+              fontSize: 15,
+            ),
+            // textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 10),
+          CustomBtn(
+            text: titleBtn,
+            status: 1,
+            onPressed: () {
+              onPress();
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
