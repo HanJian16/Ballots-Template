7b56b52a20dafb6a72c4f75cb673d16e170bc0ed
Harurin5671 <frankcito639@gmail.com>
2024-08-19 01:25:43 -0500

    feat: add formcontroller validation and redirection logic
    Implemented a function to check the FormController value and handle redirection based on the form's completion status. Redirects to the FormRegister screen if the form is not filled out.

--- frontend/ballots_template_flutter/lib/screens/home_screen.dart
+++ frontend/ballots_template_flutter/lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
+import 'package:ballots_template_flutter/controllers/form_controller.dart';
 import 'package:ballots_template_flutter/controllers/list_controller.dart';
-// import 'package:ballots_template_flutter/widgets/custom_btn.dart';
+import 'package:ballots_template_flutter/screens/form_register_screens.dart';
+import 'package:ballots_template_flutter/widgets/bottom_sheet.dart';
 import 'package:ballots_template_flutter/widgets/screen_container.dart';
+import 'package:ballots_template_flutter/widgets/warning_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -17,119 +20,41 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     Get.put(ListController());
+    Get.put(FormController());
+  }
+
+  void navigateToAnotherScreen(action) {
+    final FormController formController = Get.find();
+
+    if (formController.isFormValid) {
+      action();
+    } else {
+      Get.defaultDialog(
+        title: '',
+        titleStyle: GoogleFonts.onest(
+          fontSize: 1,
+        ),
+        content: WarningDialog(
+          title: '¡Aplicación no configurada!',
+          titleBtn: "Ir a configurar",
+          onPress: () {
+            Get.back();
+            Get.to(() => const FormRegisterScreen());
+          },
+        ),
+      );
+    }
   }
 
   void showBottomSheet(BuildContext context) {
     final ListController controller = Get.find();
+    print("Controller: ${controller.runtimeType}");
 
     showModalBottomSheet(
       context: context,
-      builder: (context) => Container(
-        height: MediaQuery.of(context).size.height * 0.9,
-        width: MediaQuery.of(context).size.width * 0.95,
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-        ),
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-                color: Colors.grey[350],
-              ),
-              child: Text(
-                'Añadir atajo',
-                style: GoogleFonts.onest(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
-            Expanded(
-              child: Obx(() {
-                final listTileData = controller.listTileData;
-                return ListView.builder(
-                  itemCount: listTileData.length,
-                  itemBuilder: (context, index) {
-                    final item = listTileData[index];
-                    return Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 12, horizontal: 20),
-                      height: 50,
-                      width: double.infinity,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            item['title'],
-                            style: GoogleFonts.onest(
-                              fontSize: 15,
-                            ),
-                          ),
-                          // Container(
-                          //   padding: const EdgeInsets.all(1.5),
-                          //   width: 20,
-                          //   height: 20,
-                          //   decoration: BoxDecoration(
-                          //     borderRadius: BorderRadius.circular(4),
-                          //     border: Border.all(
-                          //         color: const Color(0xFFAF30BF), width: 1.5),
-                          //   ),
-                          //   child: Container(
-                          //     color: item['selected']
-                          //         ? const Color(0xFFAF30BF)
-                          //         : Colors.white,
-                          //   ),
-                          // )
-                          Checkbox(
-                            value: controller.listTileData[index]['selected'],
-                            onChanged: (bool? value) {
-                              controller.updateSelection(
-                                  index, value); // Actualiza la selección
-                              // setState(() {});
-                            },
-                            activeColor:
-                                item['selected'] ? const Color(0xFFAF30BF) : Colors.white,
-                            checkColor: Colors.white,
-                            // activeColor:
-                            //     item['selected'] ? Colors.green : Colors.red,
-                            // checkColor: Colors.white,
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                );
-              }),
-            ),
-            // Container(
-            //   decoration: const BoxDecoration(
-            //     border: Border(
-            //       top: BorderSide(width: 1, color: Color(0xFFCCCCCC)),
-            //     ),
-            //   ),
-            //   child: Padding(
-            //     padding:
-            //         const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            //     child: CustomBtn(
-            //       text: 'Confirmar',
-            //       status: 1,
-            //       onPressed: () {
-            //         setState(() {});
-            //         Navigator.pop(context);
-            //       },
-            //     ),
-            //   ),
-            // )
-          ],
-        ),
+      builder: (context) => BottomSheetWidget(
+        title: 'Añadir atajo',
+        controller: controller,
       ),
     );
   }
@@ -195,7 +120,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   itemBuilder: (context, index) {
                     final item = listTileData[index];
                     return GestureDetector(
-                      onTap: () => item['onTap'](),
+                      onTap: () {
+                        navigateToAnotherScreen(item['onTap']());
+                      },
                       child: ListTile(
                         shape: const BeveledRectangleBorder(
                           borderRadius: BorderRadius.all(Radius.circular(10)),
