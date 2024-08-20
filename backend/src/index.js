ecc2a77d717506de268ee805980ef10015df6cc3
HanJian16 <jhohanjianpierre@gmail.com>
2024-08-20 17:27:28 -0500

    cambios en la configuracion inicial del backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const sequelize = require('./db');
+const { PORT } = require('./config');
 
 const app = express();
-
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
