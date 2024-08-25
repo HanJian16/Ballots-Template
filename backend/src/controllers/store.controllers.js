aa29e708a848f5fd5007ae89fead91decdf68436
Harurin5671 <frankcito639@gmail.com>
2024-08-25 15:43:35 -0500

    feat: manejar respuesta 201 y retornar el store creado en store.controller

--- backend/src/controllers/store.controllers.js
+++ backend/src/controllers/store.controllers.js
@@ -4,12 +4,10 @@ export const createStore = async (req, res) => {
   try {
     const storeData = req.body;
     console.log("Datos de recibidos:", storeData);
-    const newStore = Store.create(storeData);
-    res.status(201).json({
-      message: "Tienda creada exitosamente",
-      data: newStore,
-    });
+    const newStore = await Store.create(storeData);
+    res.status(201).json(newStore);
   } catch (error) {
+    console.log("Error al crear la tienda:", error);
     res.status(500).json({
       message: "Error al crear la tienda",
       error: error,
