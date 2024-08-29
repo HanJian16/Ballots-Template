bb7ea7ffe81ce35fec448fd32e0c7266dce5d012
Harurin5671 <frankcito639@gmail.com>
2024-08-29 18:59:42 -0500

    Agregando productInvoice, serviceinvoice en las rutas del app , creacion de las pantallas, usando las pantallas en el invoicescreen

--- frontend/ballots_template_flutter/lib/routes/app_routes.dart
+++ frontend/ballots_template_flutter/lib/routes/app_routes.dart
@@ -1,5 +1,7 @@
 import 'package:ballots_template_flutter/screens/auth/login_screen.dart';
 import 'package:ballots_template_flutter/screens/invoice/invoice_screens.dart';
+import 'package:ballots_template_flutter/screens/invoice/product_invoice_screen.dart';
+import 'package:ballots_template_flutter/screens/invoice/service_invoice_screen.dart';
 import 'package:get/get.dart';
 import 'package:ballots_template_flutter/screens/form_register_screens.dart';
 import 'package:ballots_template_flutter/screens/home_screen.dart';
@@ -9,11 +11,15 @@ class AppRoutes {
   static const home = '/home';
   static const formRegister = '/form-register';
   static const invoice = '/invoice';
+  static const productInvoice = '/invoice/product';
+  static const serviceInvoice = '/invoice/service';
 
   static final routes = [
     GetPage(name: initial, page: () => const LoginScreen()),
     GetPage(name: home, page: () => const HomeScreen()),
     GetPage(name: formRegister, page: () => const FormRegisterScreen()),
     GetPage(name: invoice, page: () => const InvoiceScreen()),
+    GetPage(name: productInvoice, page: () => const ProductInvoiceScreen()),
+    GetPage(name: serviceInvoice, page: () => const ServiceInvoiceScreen()),
   ];
 }
