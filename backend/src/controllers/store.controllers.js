d1d9e6bc35718b5155d351c189678e2d117e3ee4
Harurin5671 <frankcito639@gmail.com>
2024-08-29 00:10:19 -0500

    chore: actualizar variables de entorno, ajustar configuración de base de datos en app.js y mejorar store.controller

--- backend/src/controllers/store.controllers.js
+++ backend/src/controllers/store.controllers.js
@@ -5,11 +5,14 @@ export const createStore = async (req, res) => {
     const storeData = req.body;
     console.log("Datos de recibidos:", storeData);
     const newStore = await Store.create(storeData);
-    res.status(201).json(newStore);
+    res.status(201).json({
+      message: "Tienda creada exitosamente",
+      data: newStore,
+    });
   } catch (error) {
     console.log("Error al crear la tienda:", error);
     res.status(500).json({
-      message: "Error al crear la tienda",
+      message: error.message,
       error: error,
     });
   }
