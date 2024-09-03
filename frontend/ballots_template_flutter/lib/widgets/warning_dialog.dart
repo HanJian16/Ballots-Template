08067455efd2e9359b962c175f2aff63fee4fa91
Harurin5671 <frankcito639@gmail.com>
2024-09-03 17:25:51 -0500

    Ajustando el rutado para la lista de servicios y la boletas de servicios -Ajustando la lógica en las screens para ser reutilizadas mediante una categoría -Ajustando que use la categoría en demás funciones que se necesitan -Agregando widgets al index -Cambiando la logica del warningdialog

--- frontend/ballots_template_flutter/lib/widgets/warning_dialog.dart
+++ frontend/ballots_template_flutter/lib/widgets/warning_dialog.dart
@@ -1,52 +1,57 @@
 import 'package:flutter/material.dart';
 
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:ballots_template_flutter/theme/index.dart';
 import 'package:ballots_template_flutter/widgets/index.dart';
 
-class WarningDialog extends StatelessWidget {
-  const WarningDialog(
-      {super.key,
-      required this.title,
-      required this.onPress,
-      required this.titleBtn,
-      this.actionsBtn});
-  final String title;
-  final String titleBtn;
-  final VoidCallback onPress;
-  final Widget? actionsBtn;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 160,
-      child: Column(
-        children: [
-          const Icon(
-            Icons.warning_sharp,
-            size: 55,
-            color: AppColors.warningColor,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            title,
-            style: GoogleFonts.onest(
-              fontSize: 15,
+class WarningDialogHelper {
+  static void show({
+    String? title,
+    String? message,
+    Widget? actionsBtn,
+    VoidCallback? onPress,
+    String? titleBtn,
+  }) {
+    Get.defaultDialog(
+      title: title ?? '',
+      titleStyle: GoogleFonts.onest(
+        fontSize: 1,
+      ),
+      content: SizedBox(
+        height: 170,
+        child: Column(
+          children: [
+            const Icon(
+              Icons.warning_sharp,
+              size: 55,
+              color: AppColors.warningColor,
             ),
-            // textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 10),
-          if (actionsBtn != null) actionsBtn!,
-          if(actionsBtn == null)
-          CustomBtn(
-            text: titleBtn,
-            status: 1,
-            onPressed: () {
-              onPress();
-            },
-          ),
-        ],
+            const SizedBox(height: 10),
+            Text(
+              message ?? '',
+              style: GoogleFonts.onest(
+                fontSize: 15,
+              ),
+              // textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            if (actionsBtn != null) actionsBtn,
+            if (actionsBtn == null)
+              CustomBtn(
+                text: titleBtn ?? '',
+                status: 1,
+                onPressed: () {
+                  if (onPress != null) {
+                    onPress();
+                  } else {
+                    return;
+                  }
+                },
+              ),
+          ],
+        ),
       ),
     );
   }
