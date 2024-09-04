93555e3fb6004814e6a3184b8895cd231e94e779
HanJian16 <jhohanjianpierre@gmail.com>
2024-09-03 21:09:56 -0500

    agregando la funcionalidad a los botones para hacer el backup y la restauracion de base de datos

--- frontend/ballots_template_flutter/lib/widgets/settingsScreen/backup_options.dart
+++ frontend/ballots_template_flutter/lib/widgets/settingsScreen/backup_options.dart
@@ -1,3 +1,4 @@
+import 'package:ballots_template_flutter/db/index.dart';
 import 'package:flutter/material.dart';
 
 import 'package:ballots_template_flutter/theme/colors.dart';
@@ -13,15 +14,47 @@ class BackupOptions extends StatelessWidget {
     return Container(
       height: MediaQuery.of(context).size.height * 0.12,
       color: AppColors.whiteColor,
-      child: const Column(
+      child: Column(
         children: [
           BackupRow(
             text: "Realizar copia de seguridad",
             icon: Icons.cloud_upload,
+            onTap: () async {
+              try {
+                await backupDb();
+                NotificationHelper.show(
+                  title: 'Copia de seguridad realizada',
+                  message: 'Copia de seguridad realizada con éxito',
+                  isError: false,
+                );
+              } catch (e) {
+                NotificationHelper.show(
+                  title: 'Error',
+                  message: '$e',
+                  isError: true,
+                );
+              }
+            },
           ),
           BackupRow(
             text: "Restaurar copia de seguridad",
             icon: Icons.cloud_upload_outlined,
+            onTap: () async {
+              try {
+                await restoreDb();
+                NotificationHelper.show(
+                  title: 'Restauración realizada',
+                  message: 'Restauración realizada con éxito',
+                  isError: false,
+                );
+              } catch (e) {
+                NotificationHelper.show(
+                  title: 'Error',
+                  message: '$e',
+                  isError: true,
+                );
+              }
+            },
           ),
         ],
       ),
