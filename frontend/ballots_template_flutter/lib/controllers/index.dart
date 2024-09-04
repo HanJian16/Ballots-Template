1e56ab277db297ee08ef80ca00e6e291e485ea79
HanJian16 <jhohanjianpierre@gmail.com>
2024-09-03 21:16:48 -0500

    Eliminando productListController

--- frontend/ballots_template_flutter/lib/controllers/index.dart
+++ frontend/ballots_template_flutter/lib/controllers/index.dart
@@ -3,5 +3,4 @@ export 'list_controller.dart';
 export 'list_menu_controller.dart';
 export 'product_controller.dart';
 export 'service_controller.dart';
-export 'product_list_controller.dart';
 export 'settings_controller.dart';
